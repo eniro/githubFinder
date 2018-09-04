502753db2040d70579fea2b3aaf6b8a733a0f727
Iro Enezli <enezliro@gmail.com>
2018-09-04 20:41:05 +0300

    fixed bugs

--- docs/app.js
+++ docs/app.js
@@ -18,15 +18,14 @@ searchUser.addEventListener('keyup', (e) => {
       if(data.profile.message === 'Not Found') {
         // Show alert
         ui.showAlert('User not found', 'alert alert-danger');
-
       } else {
         // Show profile
         ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     })
   } else {
     // Clear profile
     ui.clearProfile();
-    
   }
 }); 
--- docs/github.js
+++ docs/github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = 'f0f8635ee559507ba3fb';
     this.client_secret = '3ed27a09fbd8fe310fcda0e052ac49292ba1b769';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
