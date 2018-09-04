3eaebe8075fd1219d3a63f6a93cd6878c4ffe861
Iro Enezli <enezliro@gmail.com>
2018-09-04 13:17:58 +0300

    added searchprofile function

---
+++ github.js
@@ -0,0 +1,17 @@
+class Github {
+    constructor() {
+        this.client_id = 'f0f8635ee559507ba3fb';
+        this.client_secret = '3ed27a09fbd8fe310fcda0e052ac49292ba1b769';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+
+        return {
+            profile
+        }
+    }
+
+}
